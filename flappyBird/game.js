13dc40dcec8f602d347df8b2339de8c55de4c6a7
Jameson <90803725+jamesonarnett@users.noreply.github.com>
2021-12-27 13:25:14 -0600

    Add objects/bird animations

--- flappyBird/game.js
+++ flappyBird/game.js
@@ -5,6 +5,27 @@ let frames = 0;
 const sprite = new Image();
 sprite.src = "img/sprite.png";
 
+const state = {
+  current: 0,
+  getReady: 0,
+  game: 1,
+  over: 2,
+};
+
+canvas.addEventListener("click", function (event) {
+  switch (state.current) {
+    case state.getReady:
+      state.current = state.game;
+      break;
+    case state.game:
+      bird.flap();
+      break;
+    case state.over:
+      state.current = state.getReady;
+      break;
+  }
+});
+
 const bg = {
   sX: 0,
   sY: 0,
@@ -96,21 +117,81 @@ const bird = {
       bird.sY,
       this.w,
       this.h,
-      this.x,
-      this.y,
+      this.x - this.w / 2,
+      this.y - this.h / 2,
       this.w,
       this.h
     );
+  },
+
+  flap: function () {},
+  update: function () {
+    this.period = state.current == state.getReady ? 10 : 5;
+    this.frame += frames % this.period == 0 ? 1 : 0;
+    this.frame = this.frame % this.animation.length;
+  },
+};
+
+const getReady = {
+  sX: 0,
+  sY: 228,
+  w: 173,
+  h: 152,
+  x: canvas.width / 2 - 173 / 2,
+  y: 80,
+
+  draw: function () {
+    if (state.current == state.getReady) {
+      ctx.drawImage(
+        sprite,
+        this.sX,
+        this.sY,
+        this.w,
+        this.h,
+        this.x,
+        this.y,
+        this.w,
+        this.h
+      );
+    }
+  },
+};
+
+const gameOver = {
+  sX: 175,
+  sY: 228,
+  w: 225,
+  h: 202,
+  x: canvas.width / 2 - 225 / 2,
+  y: 90,
+
+  draw: function () {
+    if (state.current == state.over) {
+      ctx.drawImage(
+        sprite,
+        this.sX,
+        this.sY,
+        this.w,
+        this.h,
+        this.x,
+        this.y,
+        this.w,
+        this.h
+      );
+    }
   },
 };
 
 function draw() {
   ctx.fillStyle = "#70c5ce";
-  ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   bg.draw();
   fg.draw();
   bird.draw();
+  bird.update();
+  getReady.draw();
+  gameOver.draw();
 }
 
 function update() {}
